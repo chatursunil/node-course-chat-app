91df498c8c60df2237837586dca7b67661adc681
Sunil Chaturvedi <Sunil Chaturvedi>
2017-01-24 16:42:52 -0500

    Add messages form and messages list

--- server/server.js
+++ server/server.js
@@ -18,9 +18,10 @@ io.on('connection', (socket) => {
 
     socket.broadcast.emit('newMessage', generateMessage('Admin', 'Hey everyone, Andrew just joined the room to get plowed'));
 
-    socket.on('createMessage', (message) => {
+    socket.on('createMessage', (message, callback) => {
         console.log('New message from client', message);
         io.emit('newMessage', generateMessage(message.from, message.text));
+        callback('This is from the server');
         // socket.broadcast.emit('newMessage', {
         //     from: message.from,
         //     text: message.text,
